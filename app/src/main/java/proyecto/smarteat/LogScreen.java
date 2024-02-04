854b3cebcc029c3d579968f00167df9d4d9cbfa6
Ulver <jculver@gmail.com>
2024-02-04 15:05:32 +0100

    Initial commit

---
+++ app/src/main/java/proyecto/smarteat/LogScreen.java
@@ -0,0 +1,14 @@
+package proyecto.smarteat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_screen);
+    }
+}
