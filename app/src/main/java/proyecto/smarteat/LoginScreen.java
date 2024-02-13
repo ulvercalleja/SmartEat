3ee7eca8a0e9e56b7613d85ebdefdb87370e26c6
Ulver <jculver@gmail.com>
2024-02-13 01:33:36 +0100

    login screen

---
+++ app/src/main/java/proyecto/smarteat/LoginScreen.java
@@ -0,0 +1,14 @@
+package proyecto.smarteat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
