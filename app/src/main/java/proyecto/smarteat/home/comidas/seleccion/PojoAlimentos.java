d64c5f6c0b6ff75653fda69de05bd1ea6390e01a
Ulver <jculver@gmail.com>
2024-11-16 17:17:33 +0100

    "api y arreglado firebase"

--- app/src/main/java/proyecto/smarteat/home/comidas/seleccion/PojoAlimentos.java
+++ app/src/main/java/proyecto/smarteat/home/comidas/seleccion/PojoAlimentos.java
@@ -6,12 +6,17 @@ public class PojoAlimentos implements Serializable {
     private int id;
     private String nombre;
     private int valorCalorico;
+    private String imagen;
 
-    public PojoAlimentos(int valorCalorico,String nombre) {
+    public PojoAlimentos(int valorCalorico,String nombre, String imagen) {
         this.nombre = nombre;
         this.valorCalorico = valorCalorico;
+        this.imagen = imagen;
     }
 
+    public String getImagen() {
+        return imagen;
+    }
     public String getNombre() {
         return nombre;
     }
